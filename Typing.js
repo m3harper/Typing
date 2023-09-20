5522b7c5eec0c821162bbaaddff6e3a96908a690
Meesha Harper <140188045+m3harper@users.noreply.github.com>
2023-09-20 13:51:45 +0100

    Add files via upload

--- Typing.js
+++ Typing.js
@@ -1,9 +1,8 @@
 // DATA /////////////////
-let score = { wins: 0, losses: 0, play: 0 }
+let score = { wins: 0, losses: 0, play: 10 }
 let game = { level: 0, timer: 0 }
 
-document.getElementById('score').innerHTML = `Wins:${score.wins} | Losses:${score.losses}`
-
+document.getElementById('score').innerHTML = `Plays: ${score.play} | Wins: ${score.wins} | Losses: ${score.losses}`
 
 const wordArray1 = [
   'above',
@@ -21,7 +20,6 @@ const wordArray1 = [
   'while',
   'wonder'
 ]
-
 
 const wordArray2 = [
   'accumilate',
@@ -65,191 +63,193 @@ const wordArray4 = [
   'trichotillamania'
 ]
 
-// EVENT LISTENERS ////////////
 
-const word = Math.floor(Math.random() * 14);
+// FUNCTIONS /////////////
 
+// set timer countdown
+let getTimer = () => {
+  
+  if (game.timer > -1 && score.play < 10) {
 
-const beginner = document.getElementById('beg-btn')
-beginner.addEventListener('click', begininerWords)
+    let getTime = setInterval(() => {
+      game.timer--
 
+      if (game.timer > -1 && score.play < 10) {
+        document.getElementById('countdown').innerText = game.timer
+        document.getElementById('countdown').classList.add('countdown')
+        document.getElementById('random-word').classList.add('random-word')
 
+      } else {
+        submitAnswer()
+        clearInterval(getTime)
+        document.getElementById('countdown').classList.remove('countdown')
+        document.getElementById('random-word').classList.remove('random-word')
+    }
+  }, 1000)
 
-const intermediate = document.getElementById('int-btn')
-intermediate.addEventListener('click', intermediateWords)
+  } else {
 
+      let getTime = setInterval(() => {
+        game.timer--
 
+        if (game.timer > -1 && score.play < 10) {
+          document.getElementById('countdown').classList.add('countdown')
+          document.getElementById('countdown').innerText = game.timer
+          document.getElementById('random-word').classList.add('random-word')
 
-const advanced = document.getElementById('adv-btn')
-advanced.addEventListener('click', advancedWords)
+        } else {
+          submitAnswer()
+          clearInterval(getTime)
+          document.getElementById('countdown').classList.remove('countdown')
+          document.getElementById('random-word').classList.remove('random-word')
+        }
+    }, 1000)
+}}
 
+// display beginner level words & save level selection to game object
+function begininerWords() {
+  const word = Math.floor(Math.random() * 14);
+  document.getElementById('random-word').innerText = `${(wordArray1[word])}`;
+  game.level = 1
+  game.timer = 7
+  changeDefault();
+  disableBtns();
+}
 
+// display intermediate level words & save level selection to game object
+function intermediateWords() {
+  const word = Math.floor(Math.random() * 14);
+  document.getElementById('random-word').innerText = `${(wordArray2[word])}`;
+  game.level = 2
+  game.timer = 6
+  changeDefault();
+  disableBtns();
+};
 
-const expert = document.getElementById('exp-btn')
-expert.addEventListener('click', expertWords)
+// display advanced level words & save level selection to game object
+function advancedWords() {
+  const word = Math.floor(Math.random() * 14);
+  document.getElementById('random-word').innerText = `${(wordArray3[word])}`;
+  game.level = 3
+  game.timer = 5
+  changeDefault();
+  disableBtns();
+};
 
- 
+// display expert level words & save level selection to game object
+function expertWords() {
+  const word = Math.floor(Math.random() * 7);
+  document.getElementById('random-word').innerText = `${(wordArray4[word])}`;
+  game.level = 4
+  game.timer = 4
+  changeDefault();
+  disableBtns();
+};
 
-const checkAnswer = document.getElementById('submit-btn')
-checkAnswer.addEventListener('click', submitAnswer)
-checkAnswer.addEventListener('click', clearBox)
+// disable level buttons
+function disableBtns() {
+  document.getElementById('beg-btn').disabled = true;
+  document.getElementById('int-btn').disabled = true;
+  document.getElementById('adv-btn').disabled = true;
+  document.getElementById('exp-btn').disabled = true;
+  document.getElementById('choose').classList.add('choose-before')
+}
 
+// enable level buttons
+function enableBtns() {
+  document.getElementById('beg-btn').disabled = false;
+  document.getElementById('int-btn').disabled = false;
+  document.getElementById('adv-btn').disabled = false;
+  document.getElementById('exp-btn').disabled = false;
+  document.getElementById('choose').classList.remove('choose-before')
+}
 
+// check answer & update score on webpage
+function submitAnswer() {
+  const myInput = document.getElementById('submit-box');
+  const currentWord = document.getElementById('random-word');
+  score.play++
+  
+  if (score.play < 11) {
+    if (currentWord.innerText === myInput.value) {
+      score.wins++
+      
+    } else {
+      score.losses++
+    } 
 
-// function to trigger submit button using Enter key
+      if (game.level === 1) {
+        begininerWords()
+      } else if (game.level === 2) {
+        intermediateWords()
+      } else if (game.level === 3) {
+        advancedWords()
+      } else if (game.level === 4) {
+        expertWords()
+    }  
+  } else { 
+   getScore()
+}  document.getElementById('score').innerText = `Wins:${score.wins} | Losses:${score.losses}`;
+};
+
+// trigger submit button using Enter key
 const input = document.getElementById('submit-box');
 input.addEventListener('keypress', function(event) {
   if (event.key === 'Enter') {
     event.preventDefault();
     document.getElementById('submit-btn').click();
-    getTimer()
   }
 })
 
-
-// FUNCTIONS /////////////
-
-// function to display beginner level words & save level selection to game object
-function begininerWords() {
-  const word = Math.floor(Math.random() * 14);
-  document.getElementById('random-word').innerHTML = `${(wordArray1[word])}`;
-  game.level = 1
-  game.timer = 7
-  getTimer();
-  changeDefault();
-}
-
-
-// function to display intermediate level words & save level selection to game object
-function intermediateWords() {
-  const word = Math.floor(Math.random() * 14);
-  document.getElementById('random-word').innerHTML = `${(wordArray2[word])}`;
-  game.level = 2
-  game.timer = 6
-  getTimer();
-  changeDefault();
-};
-
-
-// function to display advanced level words & save level selection to game object
-function advancedWords() {
-  const word = Math.floor(Math.random() * 14);
-  document.getElementById('random-word').innerHTML = `${(wordArray3[word])}`;
-  game.level = 3
-  game.timer = 5
-  getTimer();
-  changeDefault();
-};
-
-
-// function to display expert level words & save level selection to game object
-function expertWords() {
-  const word = Math.floor(Math.random() * 7);
-  document.getElementById('random-word').innerHTML = `${(wordArray4[word])}`;
-  game.level = 4
-  game.timer = 4
-  getTimer();
-  changeDefault();
-};
-
-
-// function to check answer & update score on webpage
-function submitAnswer() {
-  const myInput = document.getElementById('submit-box');
-  const currentWord = document.getElementById('random-word');
-  getTimer();
-  score.play++
-  
-  if (currentWord.innerText === myInput.value) {
-    score.wins++
-  } else {
-    score.losses++
-  }
-
-    if (game.level === 1) {
-      begininerWords()
-    } else if (game.level === 2) {
-      intermediateWords()
-    } else if (game.level === 3) {
-      advancedWords()
-    } else if (game.level === 4) {
-      expertWords()
-    } 
-  getScore()
-};
-
-// function to update submit input default text
+// update input default text
 function changeDefault() {
   document.getElementById('submit-box').setAttribute('placeholder', 'Start typing...') 
 }
 
-
-// function to clear submit field
+// clear submit field
 function clearBox() {
   document.getElementById('submit-box').value = ''
 }
 
-// function to get score and add to page
+// get score and add to page
 function getScore() {
-  if (score.play > 1 && score.losses < score.wins) {
-    reset()
-    //alert('Game over. You win!');
+  if (score.play >= 6 && score.losses < score.wins) {
+    alert('Game over. You win!');
+    score.play = 0
+    score.wins = 0
+    score.losses = 0
+    enableBtns()
     
-  } else if (score.play > 1 && score.losses > score.wins) {
-    reset()
-    //alert('Game over. You loose');
-    
+  } else { 
+    (score.play >= 6 && score.losses > score.wins) 
+    alert('Game over. You loose');
+    score.play = 0
+    score.wins = 0
+    score.losses = 0
+    enableBtns()
   }
   document.getElementById('score').innerText = `Wins:${score.wins} | Losses:${score.losses}`
-  
 }
 
+// EVENT LISTENERS ////////////
+const word = Math.floor(Math.random() * 14);
 
-// function to set timer countdown
-let timeBreak = false;
+const beginner = document.getElementById('beg-btn')
+beginner.addEventListener('click', begininerWords)
+beginner.addEventListener('click', getTimer)
 
-function getTimer() {
-  
-  if (!timeBreak) {
-    timer = setInterval(function function1() {
+const intermediate = document.getElementById('int-btn')
+intermediate.addEventListener('click', intermediateWords)
+intermediate.addEventListener('click', getTimer)
 
-      document.getElementById('countdown').classList.remove('black-out')
-      document.getElementById('countdown').classList.add('countdown')
-      document.getElementById("countdown").innerText = game.timer
-      game.timer--;
+const advanced = document.getElementById('adv-btn')
+advanced.addEventListener('click', advancedWords)
+advanced.addEventListener('click', getTimer)
 
-      if (game.timer < 0) {
-        submitAnswer();
-        setInterval(timer);
-      }
-
-    }, 1000);
-
-    // stops the above if statement running. Without stopping this our timer would continue running and another timer would run / run very fast!
-    timeBreak = true;
-
-  } else {
-    setInterval(timer);
-
-  } 
-}
-
-
-function reset() {
-  score.wins = 5
-  score.losses = 0
-  score.play = 0
-  game.level = 0
-  game.timer = 0
-  const beginner = document.getElementById('beg-btn')
-  beginner.removeEventListener('click', begininerWords)
-
-  const intermediate = document.getElementById('int-btn')
-  intermediate.removeEventListener('click', intermediateWords)
-
-  const advanced = document.getElementById('adv-btn')
-  advanced.removeEventListener('click', advancedWords)
-
-  const expert = document.getElementById('exp-btn')
-  expert.removeEventListener('click', expertWords)
-}
+const expert = document.getElementById('exp-btn')
+expert.addEventListener('click', expertWords)
+expert.addEventListener('click', getTimer)
+ 
+const checkAnswer = document.getElementById('submit-btn')
+checkAnswer.addEventListener('click', submitAnswer)
+checkAnswer.addEventListener('click', clearBox)
